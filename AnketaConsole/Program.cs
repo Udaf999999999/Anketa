63071fdacf830da5405cd13203ba78adb2e93f3b
Udaf999999999 <udaf999999999@yandex.ru>
2024-12-07 05:05:42 +0700

    Add FillAnketa

--- AnketaConsole/Program.cs
+++ AnketaConsole/Program.cs
@@ -2,8 +2,88 @@
 {
     internal class Program
     {
+        delegate void CoutMessage();
+        private static string[] FillPetNames(int petCount)
+        {
+            string[] petNames = new string[petCount];
+            for (int i = 0; i < petCount; i++)
+            {
+                Console.WriteLine("Enter your " + (i + 1) + " pet's name:");
+                petNames[i] = Console.ReadLine();
+            }
+            return petNames;
+        }
+        private static string[] FillFavoriteColor(int favoriteColorCount)
+        {
+            string[] favoriteColors = new string[favoriteColorCount];
+            for (int i = 0; i < favoriteColorCount; i++)
+            {
+                Console.WriteLine("Enter your " + (i + 1) + " favorite color:");
+                favoriteColors[i] = Console.ReadLine();
+            }
+            return favoriteColors;
+        }
+        private static int CheckCorrectInput(bool isCorrect,int number, string typeVar, CoutMessage coutMessage)
+        {
+            while (!isCorrect || number <= 0)
+            {
+                if (!isCorrect) Console.WriteLine("It's not a number");
+                if (number <= 0) Console.WriteLine(typeVar + "can't be under 1");
+                coutMessage();
+                isCorrect = int.TryParse(Console.ReadLine(), out number);
+            }
+            return number;
+        }
+        private static void FillAnketa()
+        {
+            Console.WriteLine("Enter your name:");
+            String userName = Console.ReadLine();
+            Console.WriteLine("Enter your surname:");
+            String surName = Console.ReadLine();
+            CoutMessage coutMessage = () => Console.WriteLine("Enter your age:");
+            coutMessage();
+            int age = 0;
+            bool isAgeCorrect = int.TryParse(Console.ReadLine(), out age);
+            if (!isAgeCorrect || age <= 0)
+            {
+                age = CheckCorrectInput(isAgeCorrect, age, "Age", coutMessage);
+            }
+
+            Console.WriteLine("Do you have a pet? Yes/No");
+            String answer = Console.ReadLine();
+            bool havePet = answer == "Yes" ? true : false;
+            int petCount = 0;
+            if (havePet)
+            {
+                coutMessage = () => Console.WriteLine("How many pets do you have?");
+                bool isPetCountCorrect = int.TryParse(Console.ReadLine(), out petCount);//Обработать исключение
+                if (!isPetCountCorrect || petCount <= 0)
+                {
+                    age = CheckCorrectInput(isAgeCorrect, age, "Pet count", coutMessage);
+                }
+            }
+            string[] petNames;
+            if (petCount > 0)
+            {
+                petNames = FillPetNames(petCount);
+            }
+            Console.WriteLine("What the count of your favorite colors?");
+            int favoriteColorCount = 0;
+            bool isCorrectFavoriteColorCount = int.TryParse(Console.ReadLine(), out favoriteColorCount);
+            string[] favoriteColors;
+            if (favoriteColorCount > 0)
+            {
+
+                favoriteColors = FillFavoriteColor(favoriteColorCount);
+            }
+
+
+
+
+        }
         static void Main(string[] args)
         {
+            FillAnketa();
             Console.WriteLine("Hello, World!");
         }
     }
