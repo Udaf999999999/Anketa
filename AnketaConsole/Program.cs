c41c5e0c311c5330b682690a20284ad3100278d5
Udaf999999999 <udaf999999999@yandex.ru>
2024-12-07 04:15:30 +0700

    Add solution

---
+++ AnketaConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AnketaConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
